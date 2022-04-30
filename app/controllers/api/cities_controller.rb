83ca4b32203a3d71aa4d88915fb1f1750743549e
Bill Kim <ywbkdev@gmail.com>
2022-04-30 16:22:42 -0400

    add useState and search onChange to Home component

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -5,13 +5,11 @@ class Api::CitiesController < ApplicationController
 
   def show
     city = params[:city]
-    # debugger
     key = Rails.application.credentials.open_weather[:api_key]
     url = "http://api.openweathermap.org/geo/1.0/direct?q=#{city}&limit=5&appid=#{key}"
     res = RestClient.get(url)
     parsed = JSON.parse(res.body)
     parsed.map! { |city| city.except("local_names") }
-    # debugger
     render json: parsed
   end
 end
--- frontend/components/home.js
+++ frontend/components/home.js
@@ -2,12 +2,16 @@ import React from "react";
 import Link from "react-router-dom"
 
 const Home = () => {
+  const [searchStr, setSearchStr] = React.useState("");
+
   return (
     <React.Fragment>
       <h1>Weather App</h1>
       <input 
         type="text" 
-        placeholder="Search city" />
+        placeholder="Search city"
+        value={searchStr}
+        onChange={ (e) => setSearchStr(e.currentTarget.value) } />
       <input 
         type="submit"
         value="Search" />
