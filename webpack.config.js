cfb87da85a2f3eb06ecbb62d38a801749f5d4803
Bill Kim <ywbkdev@gmail.com>
2022-04-29 23:44:05 -0400

    setup skeleton

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/weather_app.jsx', 
+  output: { 
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: 'bundle.js',
+    assetModuleFilename: '../images/[hash][ext][query]'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      },
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
