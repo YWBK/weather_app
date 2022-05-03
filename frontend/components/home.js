3d81cd9df96a1c81ca279419ac623bacf52dde11
Bill Kim <ywbkdev@gmail.com>
2022-05-03 15:32:41 -0400

    add modal that contains search bar to change city on card

--- frontend/components/home.js
+++ frontend/components/home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Navbar from "./navbar";
 import CityCard from "./city_card";
+import createStore from "runtime-memcache";
 
 const Home = () => {
+  // const config = {
+  //   policy: "lru",
+  //   lruSize: 5,
+  // };
+
+  // const userCache = createStore(config);
+
   const nyc = [
-    "Manhattan, NY", 
-    "Queens, NY", 
-    "Brooklyn, NY", 
-    "Bronx, NY", 
+    // "Manhattan, NY", 
+    // "Queens, NY", 
+    // "Brooklyn, NY", 
+    // "Bronx, NY", 
     "Staten Island, NY"];
 
   return (
