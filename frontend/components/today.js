433efe10e4218b62a0539a493987dbe80d4abf84
Bill Kim <ywbkdev@gmail.com>
2022-05-04 18:58:26 -0400

    add icons to show page

--- frontend/components/today.js
+++ frontend/components/today.js
@@ -1,8 +1,7 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
 
 const Today = ({ cityName, weather }) => {
-  // debugger
   return (
     <React.Fragment>
       <div className='detail-title'>
@@ -18,29 +17,71 @@ const Today = ({ cityName, weather }) => {
           { weather.main }
         </li>
         <li className='detail-item'>
-          <p>High / Low { weather.high}° / { weather.low }°</p>
+          <p className='detail-high-low'>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-temperature-high"
+              className="icon" /> 
+            {weather.high}° /
+          </p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-temperature-low"
+              className="icon" />
+            { weather.low }°
+          </p>
         </li>
       </ul>
       <ul className='today-detail'>
         <li className='detail-item'>
           <p className='detail-caption'>Sunrise / Sunset</p>
-          <p>{ weather.sunrise} / { weather.sunset }</p>
+          <p className='detail-high-low'>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-sun"
+              className="icon" />
+            { weather.sunrise} / 
+          </p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-moon"
+              className="icon" />
+            { weather.sunset }
+          </p>
         </li>
         <li className='detail-item'>
           <p className='detail-caption'>Wind</p> 
-          <p>{ weather.windDir} { weather.windSpeed} mph</p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-wind"
+              className="icon" />
+            { weather.windDir} { weather.windSpeed} mph
+          </p>
         </li>
         <li className='detail-item'>
           <p className='detail-caption'>Humidity</p> 
-          <p>{ weather.humidity } %</p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-water"
+              className="icon" />
+            { weather.humidity } %
+          </p>
         </li>
         <li className='detail-item'>
           <p className='detail-caption'>Visibility</p>
-          <p>{ weather.visibility } km</p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-eye"
+              className="icon" />
+            { weather.visibility } km
+          </p>
         </li>
         <li className='detail-item'>
           <p className='detail-caption'>Pressure</p>
-          <p>{ weather.pressure } hPa</p>
+          <p>
+            <FontAwesomeIcon 
+              icon="fa-solid fa-arrows-down-to-line"
+              className="icon" />
+            { weather.pressure } hPa
+          </p>
         </li>
       </ul>
     </React.Fragment>
